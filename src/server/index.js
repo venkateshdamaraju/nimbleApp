6e7e39e567f3e258790b3257bc06036a79b48744
Venkatesh Damaraju <damaraju.venkatesh761@gmail.com>
2025-05-16 13:35:24 -0400

    Initial Setup

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Worker } from 'worker_threads';
+import { handleWebRTC } from './webrtcHandler.js';
+import { setupWebTransport } from './transportHandler.js';
+
+const app = express();
+const server = createServer(app);
+const port = 3000;
+
+app.use(express.static('../client'));
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+setupWebTransport(server, handleWebRTC);
